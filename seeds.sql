3c9722bee861ef229fd8b822aa1a528679e8e16e
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-10-12 22:52:45 -0700

    stuff

--- seeds.sql
+++ seeds.sql
@@ -1,23 +1,7 @@
-USE VG_Nexus_db;
+USE q1u9gnricx1xg22v;
 
-INSERT INTO Posts (text, ThreadId, UserId, createdAt, updatedAt) VALUES
-("Man, I'm sorry, I honestly can't think of any announcements at this point. We'll see...", 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("bump", 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("dude, don't bump announcement threads.", 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-
-("I made a game! blah blah blah blah blah...", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("I cannot believe how unprofessional this website is. I loaded it up and now my computer has a million viruses on1 n23i it i 1234c acant 2324234halp34234halp please1231111", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 3, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("excuse me?", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("I made a game! blah blah blah blah blah...", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("I cannot believe how unprofessional this website is. I loaded it up and now my computer has a million viruses on1 n23i it i 1234c acant 2324234halp34234halp please1231111", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 3, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("excuse me?", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("Where can you get gas for $1.49?", 4, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("where", 4, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("taco bell", 4, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("lol", 4, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+INSERT INTO Tags (name,  createdAt, updatedAt) VALUES
+("Action", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+("Arcade", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+("Shooter", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+("Role Playing", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 
 // if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 // }
 // else{
 //   app.use(express.static("client/public/user"));
