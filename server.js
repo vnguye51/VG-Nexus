0685abbdb5b3093559a9c05fffec2cca46c6fd47
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-10-12 19:22:36 -0700

    created build

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 3001;
 var passport = require("./config/passport");
 var db = require("./models");
 var session = require("express-session");
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -18,21 +17,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
-else{
-  app.use(express.static("client/public/user"));
-}
+// }
+// else{
+//   app.use(express.static("client/public/user"));
+// }
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 
 require("./routes/api-routes.js")(app,io)
 
 
 const routes = require("./routes/sequelize-template-routes.js")
+app.get('/*',function(req,res){
+  res.sendFile(__dirname+ '/client/build/index.html')
+})
 app.use(routes)
 // Start the API server
 db.sequelize.sync().then(function(){
